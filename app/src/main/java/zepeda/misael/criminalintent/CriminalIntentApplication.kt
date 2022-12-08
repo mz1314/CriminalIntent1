10feb788589b7413fd8a7d974993abc7341a9a19
Misael <dan.zepeda@alumnos.udg.mx>
2022-12-08 01:08:03 -0600

    hasta aqui

---
+++ app/src/main/java/zepeda/misael/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package zepeda.misael.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
